ee3a62e1f36b0a1fce0a15cfaae04fe74af5f785
Rickard Garnau <rickard.garnau89@gmail.com>
2026-01-04 13:32:12 +0100

    Initial upload

---
+++ exercises/exercise_2/src/os_data.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+import matplotlib as plt
+
+print("\n")
+print(f"Sys version: {sys.version}")
+print("\n")
+print(f"Pandas version: {pd.__version__}")
+print("\n")
+print(f"Mathplotlib version: {plt.__version__}")
